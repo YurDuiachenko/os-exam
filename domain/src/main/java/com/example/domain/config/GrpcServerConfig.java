63c96b80acb468d2b91294969e811445b44e5b1b
YurDuiachenko <d.yurok22@mail.ru>
2025-01-01 18:51:26 +0300

    add env vars

--- domain/src/main/java/com/example/domain/config/GrpcServerConfig.java
+++ domain/src/main/java/com/example/domain/config/GrpcServerConfig.java
@@ -3,14 +3,18 @@ package com.example.domain.config;
 import com.example.domain.grpc.BookGrpcService;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class GrpcServerConfig {
+    @Value("${grpc.server.port}")
+    private int grpcPort;
+
     @Bean
     public Server grpcServer(BookGrpcService bookGrpcService) {
-        return ServerBuilder.forPort(50051)
+        return ServerBuilder.forPort(grpcPort)
                 .addService(bookGrpcService)
                 .build();
     }
